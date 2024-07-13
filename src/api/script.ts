004daddde197bf92e65e7acbff9febfd5c275fff
miyazaki1707 <miyazaki1707@gmail.com>
2024-07-13 17:55:37 +0700

    env update

--- src/api/script.ts
+++ src/api/script.ts
@@ -1,13 +1,10 @@
 // Import the functions you need from the SDKs you need
-import dotenv from 'dotenv';
-//import 'node';
-import 'process';
 import { initializeApp } from "firebase/app";
 import { getFirestore, setDoc, doc, collection, getDocs, query, deleteDoc } from "firebase/firestore";
 import { Goal } from "../store/goals/goals.interface";
 import { IFilter } from "../store/filters/filtersSlice";
 
-dotenv.config();
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,13 +12,13 @@ dotenv.config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREABSE_MEASUREMENTID,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREABSE_MEASUREMENTID,
 };
 
 // Initialize Firebase
